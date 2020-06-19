2fda350575c8049d231969d76b5ddf3a602c6b61
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-06-19 19:40:41 -0300

    :construction: Creates the migration of Pixelthon participants

---
+++ database/migrations/1592606133548_pixelthon_participants_schema.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class PixelthonParticipantsSchema extends Schema {
+  up() {
+    this.create('pixelthon_participants', (table) => {
+      table.increments()
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('pixelthon_participants')
+  }
+}
+
+module.exports = PixelthonParticipantsSchema
