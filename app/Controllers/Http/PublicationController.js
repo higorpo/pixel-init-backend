78a5f88015aab018078c8fa7734ea77d88ac4b86
Higor Oliveira <higorpiresoliveira@gmail.com>
2020-06-22 14:25:39 -0300

    :sparkles: Create posts

--- app/Controllers/Http/PublicationController.js
+++ app/Controllers/Http/PublicationController.js
@@ -26,7 +26,7 @@ class PublicationController {
 		const publications = await Publication
 			.query()
 			.select('*')
-			.select(Database.raw(`IF((SELECT id FROM publication_likes WHERE publication_likes.publication_id = 1 AND publication_likes.user_id = ? LIMIT 1) IS NOT NULL, true, false) as is_liked`, [auth.user.id]))
+			.select(Database.raw(`IF((SELECT id FROM publication_likes WHERE publication_likes.publication_id = publications.id AND publication_likes.user_id = ? LIMIT 1) IS NOT NULL, true, false) as is_liked`, [auth.user.id]))
 			.with('author')
 			.withCount('likes')
 			.withCount('comments')
@@ -37,7 +37,39 @@ class PublicationController {
 		return publications;
 	}
 
-	async store({ request, response }) {
+	async store({ request, response, auth }) {
+		const rules = {
+			text: 'required|string',
+		}
+
+		const validation = await validate(request.raw(), rules)
+
+		if (validation.fails()) {
+			return response.status(400).send(validation.messages())
+		}
+
+		const { text } = request.all();
+
+		try {
+			const NewPost = await Publication.create({
+				user_id: auth.user.id,
+				text: text
+			})
+
+			const publication = await Publication
+				.query()
+				.select('*')
+				.select(Database.raw(`IF((SELECT id FROM publication_likes WHERE publication_likes.publication_id = publications.id AND publication_likes.user_id = ? LIMIT 1) IS NOT NULL, true, false) as is_liked`, [auth.user.id]))
+				.with('author')
+				.withCount('likes')
+				.withCount('comments')
+				.where('id', NewPost.id)
+				.first();
+
+			return response.status(201).send(publication);
+		} catch (error) {
+			return response.status(500).send({ error });
+		}
 	}
 
 	async show({ params, request, response, view }) {
